f3be1f1b0425d5df20e1e491679213d0f77443f6
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-24 16:28:11 +0530

    created model

--- MyTrain/trainapp/models.py
+++ MyTrain/trainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Train(models.Model):
+
+    def __str__(self):
+        return f"{self.Train_name} ({self.train_code})"
+    
+    Train_name = models.CharField(max_length=50)
+    train_code = models.IntegerField(blank=True)
+    total_capacity = models.IntegerField()
+
+
+class Station(models.Model):
+
+
+    station_name = models.CharField(max_length=50)
+    station_code = models.IntegerField()
+
+
+class Schedule(models.Model):
+
+
+    train = models.ForeignKey(Train,on_delete=models.CASCADE)
+    source = models.ForeignKey(Station,related_name='departure',on_delete=models.CASCADE)
+    destination = models.ForeignKey(Station,related_name='arrival',on_delete=models.CASCADE)
+    departure = models.DateTimeField()
+    available_seat = models.IntegerField()
+
