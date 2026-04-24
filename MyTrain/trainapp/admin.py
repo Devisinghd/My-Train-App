bbd78744edebabbbf655b39fdc81b37311b7ef93
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-24 16:27:01 +0530

    registerd model

--- MyTrain/trainapp/admin.py
+++ MyTrain/trainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Train , Station , Schedule
 # Register your models here.
+admin.site.register(Train)
+admin.site.register(Station)
+admin.site.register(Schedule)
