a9a0285dcde39b90d1c2a351e09e673c0fd4ef6b
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-24 16:29:34 +0530

    created views

--- MyTrain/trainapp/views.py
+++ MyTrain/trainapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Train
 # Create your views here.
+def Main(request):
+    trains = Train.objects.all()
+    return render(request,'trainapp/index.html',{'trains':trains})
